b27079e50eb57ce593149331ac9d88e219e29731
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2021-07-15 17:04:18 -0700

    add etheral support to test mail sending for resetting password

---
+++ lib/mail.ts
@@ -0,0 +1,61 @@
+import { createTransport, getTestMessageUrl } from 'nodemailer';
+
+const transport = createTransport({
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
+  },
+});
+
+function makeANiceEmail(text: string): string {
+  return `
+  <div style=
+  border: 1px solid black;
+  padding: 20px;
+  font-family: sans-serif;
+  line-height: 2;
+  font-size: 20px;
+  ">
+  <h2>Hello There!</h2>
+  <p>${text}</p>
+  <p>😊, Admin</p>
+  </div>
+  `;
+}
+
+export interface Envelope {
+  from: string;
+  to?: string[] | null;
+}
+
+export interface MailResponse {
+  accepted?: string[] | null;
+  rejected?: null[] | null;
+  envelopeTime: number;
+  messageTime: number;
+  messageSize: number;
+  response: string;
+  envelope: Envelope;
+  messageId: string;
+}
+
+export async function sendPasswordResetEmail(
+  resetToken: string,
+  to: string,
+): Promise<void> {
+  // email the user a token
+  const info = (await transport.sendMail({
+    to,
+    from: 'andrew.lloyd01@googlemail.com',
+    subject: 'Your Password Reset Token!',
+    html: makeANiceEmail(`Your Password Reset Token is here!
+    
+    <a href="${process.env.FRONTEND_URL}/reset?token=${resetToken}">Click Here To reset</a>
+    `),
+  })) as MailResponse;
+  if (process.env.MAIL_USER.includes('ethereal.email')) {
+    console.log(`✉️ Message Sent! Preview it at ${getTestMessageUrl(info)}`);
+  }
+}
