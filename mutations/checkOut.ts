6450670f95cc696c22582022170a639d1726a2e4
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2021-07-25 15:10:41 -0700

    create order and orderItems in checkOut mutation

--- mutations/checkOut.ts
+++ mutations/checkOut.ts
@@ -74,7 +74,33 @@ async function checkOut(
 
   console.log(charge);
   // 4. Convert the cartItems to orderItems
+  const orderItems = cartItems.map((cartItem) => {
+    const orderItem = {
+      name: cartItem.product.name,
+      description: cartItem.product.description,
+      price: cartItem.product.price,
+      quantity: cartItem.quantity,
+      photo: { connect: { id: cartItem.product.photo.id } },
+    };
+    return orderItem;
+  });
   // 5. Create the order and return it
+  const order = await context.lists.Order.createOne({
+    data: {
+      total: charge.amount,
+      charge: charge.id,
+      items: { create: orderItems },
+      user: { connect: { id: userId } },
+    },
+  });
+  console.log(order);
+  // 6. Clean up any old cart items
+  const cartItemIds = cartItems.map((cartItem) => cartItem.id);
+  await context.lists.CartItem.deleteMany({
+    ids: cartItemIds,
+  });
+  console.log(order);
+  return order;
 }
 
 export default checkOut;
